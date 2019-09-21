8a9098ce32fb53eedbf8f38476c79333e9bba8ad
mrdycstln18 <40745771+mrdycstln18@users.noreply.github.com>
2019-09-21 21:54:00 +0800

    Add files via upload

--- config.php
+++ config.php
@@ -1,27 +1,16 @@
 <?php
-// /* Database credentials. Assuming you are running MySQL
-// server with default setting (user 'root' with no password) */
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $database = "thesis_db";
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "thesis_db";
  
-//  Attempt to connect to MySQL database 
-// $conn = mysqli_connect($servername, $username, $password, $database);
+/* Attempt to connect to MySQL database */
+$conn = mysqli_connect($servername, $username, $password, $database);
  
-// // Check connection
-// if(!$conn){
-// 	die("Database connection failed".mysql_connect_error());
-// }
-// else{
-// 	//echo "connected!!!";
-// }
-$connections = mysqli_connect("localhost","root","","thesis_db");
-if (mysqli_connect_errno()){
-	echo "Failed to Connect to MySQL: " . mysqli_connect_error();
+// Check connection
+if(!$conn){
+	die("Database connection failed".mysql_connect_error());
 }
-else{
-	echo "";
-}
-
 ?>
